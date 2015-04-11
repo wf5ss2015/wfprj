ef8f8182278f167da31dae1e8c8ea46bc32b008f
wysdam <wysdam@users.noreply.github.com>
2015-04-11 07:49:22 +0200

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,19 +1,18 @@
 <?php
-/*
-* HS­Ulm, WF5, SS2015, Prof. Klippel, Wirtschaftsinformatik­Projekt
-* Projekt: Lehrveranstaltungssoftware
-* Name: Kilian Kraus
-* Gruppe: 01
-* Version: 1
+/**
+* SPRINT 01
+*
+* @author: Kilian Kraus
+* @Matrikel:
 * Datum: 08.04.2015
 *
-* User­Story (Nr. 20 ): Als Dozent möchte ich mich zur Verwaltung meiner Daten online einloggen können. (42 Points)
+* UserÂ­Story (Nr. 20 ): Als Dozent mÃ¶chte ich mich zur Verwaltung meiner Daten online einloggen kÃ¶nnen. (42 Points)
 * Zeit:
 */
 ?>
 <?php
 // @author Kilian Kraus
-// lädt einmal die application.php. Dieses dient als Startpunkt der Anwendung.
+// lÃ¤dt einmal die application.php. Dieses dient als Startpunkt der Anwendung.
 require_once '../application/core/Application.php';
 
 // Startet die Anwendung.
