27b4d6c11b506849671ff476084a8893e69b2c83
wysdam <wysdam@users.noreply.github.com>
2015-04-11 07:47:21 +0200

    Update loggedin.php

--- application/view/login/loggedin.php
+++ application/view/login/loggedin.php
@@ -1,10 +1,9 @@
 <?php
-/*
-* HS­Ulm, WF5, SS2015, Prof. Klippel, Wirtschaftsinformatik­Projekt
-* Projekt: Lehrveranstaltungssoftware
-* Name: Kilian Kraus
-* Gruppe: 01
-* Version: 1
+/**
+* SPRINT 01
+*
+* @author: Kilian Kraus
+* @Matrikel:
 * Datum: 08.04.2015
 *
 * User­Story (Nr. 20 ): Als Dozent möchte ich mich zur Verwaltung meiner Daten online einloggen können. (42 Points)
