9bb036baac3a893f4c7642464fc6e5e0d03f2c0b
wysdam <wysdam@users.noreply.github.com>
2015-04-11 07:45:31 +0200

    Update UserModel.php

--- application/model/UserModel.php
+++ application/model/UserModel.php
@@ -1,13 +1,12 @@
 <?php
-/*
-* HS­Ulm, WF5, SS2015, Prof. Klippel, Wirtschaftsinformatik­Projekt
-* Projekt: Lehrveranstaltungssoftware
-* Name: Kilian Kraus
-* Gruppe: 01
-* Version: 1
+/**
+* SPRINT 01
+*
+* @author: Kilian Kraus
+* @Matrikel:
 * Datum: 08.04.2015
 *
-* User­Story (Nr. 20 ): Als Dozent möchte ich mich zur Verwaltung meiner Daten online einloggen können. (42 Points)
+* UserÂ­Story (Nr. 20 ): Als Dozent mÃ¶chte ich mich zur Verwaltung meiner Daten online einloggen kÃ¶nnen. (42 Points)
 * Zeit: 0.5
 */
 ?>
@@ -15,7 +14,7 @@
 <?php
 /**
  * @author Kilian Kraus
- * Model für den Nutzer. In diesem fall nur um Nutzerdaten anhand des Nutzernames zu bekommen.
+ * Model fÃ¼r den Nutzer. In diesem fall nur um Nutzerdaten anhand des Nutzernames zu bekommen.
  */
 class UserModel
 {
@@ -24,7 +23,7 @@ class UserModel
      *
      * @param $user_name string Nutzername
      *
-     * @return mixed Gibts false zurück, wenn Nutzer nicht besteht. Ansonsten Objekt mit den Nutzerdaten zurück.
+     * @return mixed Gibts false zurÃ¼ck, wenn Nutzer nicht besteht. Ansonsten Objekt mit den Nutzerdaten zurÃ¼ck.
      */
     public static function getUserData($user_name)
     {
