61e1fd5a5b9ee5fdfa85e0b7fcdc0226bf19a605
wysdam <wysdam@users.noreply.github.com>
2015-04-11 07:46:42 +0200

    Update index.php

--- application/view/index/index.php
+++ application/view/index/index.php
@@ -1,13 +1,12 @@
 <?php
-/*
-* HS­Ulm, WF5, SS2015, Prof. Klippel, Wirtschaftsinformatik­Projekt
-* Projekt: Lehrveranstaltungssoftware
-* Name: Kilian Kraus
-* Gruppe: 01
-* Version: 1
+/**
+* SPRINT 01
+*
+* @author: Kilian Kraus
+* @Matrikel:
 * Datum: 08.04.2015
 *
-* User­Story (Nr. 20 ): Als Dozent möchte ich mich zur Verwaltung meiner Daten online einloggen können. (42 Points)
+* UserÂ­Story (Nr. 20 ): Als Dozent mÃ¶chte ich mich zur Verwaltung meiner Daten online einloggen kÃ¶nnen. (42 Points)
 * Zeit: 
 */
 ?><!--TODO HTML5 umsetzen-->
