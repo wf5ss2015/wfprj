694df8d9713732d4bd897abac9f5c414a5a45ffc
wysdam <wysdam@users.noreply.github.com>
2015-04-11 07:44:53 +0200

    Update View.php

--- application/core/View.php
+++ application/core/View.php
@@ -1,13 +1,12 @@
 <?php
-/*
-* HS≠Ulm, WF5, SS2015, Prof. Klippel, Wirtschaftsinformatik≠Projekt
-* Projekt: Lehrveranstaltungssoftware
-* Name: Kilian Kraus
-* Gruppe: 01
-* Version: 1
+/**
+* SPRINT 01
+*
+* @author: Kilian Kraus
+* @Matrikel:
 * Datum: 08.04.2015
 *
-* User≠Story (Nr. 20 ): Als Dozent mˆchte ich mich zur Verwaltung meiner Daten online einloggen kˆnnen. (42 Points)
+* User¬≠Story (Nr. 20 ): Als Dozent m√∂chte ich mich zur Verwaltung meiner Daten online einloggen k√∂nnen. (42 Points)
 * Zeit: 0.1
 */
 ?>
@@ -16,18 +15,18 @@
 
 /**
  * @author Kilian Kraus
- * Diese Klasse ist f¸r den Output da.
+ * Diese Klasse ist f√ºr den Output da.
  * view->render('Ordnername/Dateiname')
  */
 class View
 {
     public function render($filename, $data = null)
     {
-		// l‰dt den header
+		// l√§dt den header
         require Config::get('PATH_VIEW') . '_templates/header.php';
-		// l‰dt den content
+		// l√§dt den content
         require Config::get('PATH_VIEW') . $filename . '.php';
-		// l‰dt den footer
+		// l√§dt den footer
         require Config::get('PATH_VIEW') . '_templates/footer.php';
     }
  
