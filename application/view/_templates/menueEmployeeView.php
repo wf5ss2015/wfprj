c3b7c679d9796376059c2136a3472091b18b54e5
kklamser <kklamser@mail.hs-ulm.de>
2015-05-19 18:54:37 +0200

    nutzerliste in menu aufgenommen

--- application/view/_templates/menueEmployeeView.php
+++ application/view/_templates/menueEmployeeView.php
@@ -9,6 +9,12 @@
 * User­Story (Nr. 20 ): Als Entwickler möchte ich eine einheitliche Optik und einen einheitlichen Aufbau der Webseiten haben. (Nacharbeit)   (13 Points)
 * Zeit: 0.5
 */
+/*
+	Erweiterung um Menüpunkt Nutzer:
+	autor: Kris Klamser
+	sprint: 4
+	zeit: 0.1
+*/
 -->
 
 <!-- Individuelle menübar -->
@@ -30,6 +36,25 @@
 				<li><a href="index.php?url=raumplan/erzeugeFormular1">Raumplan
 						anzeigen</a></li>
 			</ul></li>
+		<?php 
+			/* 	Start Änderungen 
+				autor: Kris Klamser
+				datum: 19.5.2015
+				sprint: 4
+			*/
+		?>
+		<li><a href="#">Nutzer</a>
+			<ul>
+				<li><a href="index.php?url=nutzerlistedrucken/nutzerlistedrucken">Nutzerliste anzeigen</a></li>
+			</ul>
+		</li>
+		<?php 
+			/* 	Ende Änderungen 
+				autor: Kris Klamser
+				datum: 19.5.2015
+				sprint: 4
+			*/
+		?>
 		<li><a href="index.php?url=login/logout">Ausloggen</a></li>
 		<li style="float: right;"><a href="index.php?url=login/index">Login</a>
 		</li>
