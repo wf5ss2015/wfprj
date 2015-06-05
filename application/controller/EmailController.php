436fbfc90d447fe4b89a2498f8c36a22397e56d5
wysdam <wysdam@googlemail.com>
2015-06-05 22:27:30 +0200

    Aufräumen
    2015-06-05 20:24:26 SMTP ERROR: Failed to connect to server: Es konnte
    keine Verbindung hergestellt werden, da der Zielcomputer die Verbindung
    verweigerte. WHAT THE FUCK

--- application/controller/EmailController.php
+++ application/controller/EmailController.php
@@ -32,101 +32,105 @@ class EmailController extends Controller {
 	}
 	
 	public function sendMailDozent(){
-		$mail = new PHPMailer();
- 
-	echo Config::get('smtp_host');
-  //Absenderadresse der Email setzen
-  $mail->From = Config::get('smtp_email');
-  
-  //Name des Abenders setzen
-  $mail->FromName = Session::get('user_name');
-  
-  //Empfängeradresse setzen
-  $mail->AddAddress("kraus.kilian@googlemail.com");
-  
-  //Betreff der Email setzen
-  $mail->Subject = "Test1234";
- 
-  //Text der EMail setzen
-  $mail->Body = "Hallo! \n\n Dies ist die erste Email mit PHPMailer! Viele Grüße".Session::get('user_name');
-  
-  $mail->IsSMTP(); //Versand über SMTP festlegen
-  $mail->Host = Config::get('smtp_host'); //SMTP-Server setzen
-  
-  $mail->SMTPAuth = true;     //Authentifizierung aktivieren
-  $mail->Username = Config::get('smtp_user');  // SMTP Benutzername
-  $mail->Password = Config::get('smtp_pass'); // SMTP Passwort 
-  
-  //EMail senden und überprüfen ob sie versandt wurde
-  if(!$mail->Send())
-  {
-      Session::add('response_negative', 'Email wurde nicht versandt. Es ist ein Fehler aufgetreten: '.$mail->ErrorInfo);
-  }
-  else
-  {
-     Session::add('response_positive', 'Email wurde versandt.');
-}
-Redirect::to('dozent/emailDozent');
-}
+	
+		$mail = new PHPMailer(true);
 
+		$mail->IsSMTP(); //Versand über SMTP festlegen
+		
+		try{
+			$mail->Host = Config::get('smtp_host'); //SMTP-Server setzen
+			$mail->SMTPDebug  = 2; // Debug information
+			
+			// Testing
+			$mail->SMTPAuth = true; //Authentifizierung aktivieren
+			$mail->Port = Config::get('smtp_port');
+			
+			// Server
+			$mail->Username = Config::get('smtp_user');  // SMTP Benutzername
+			$mail->Password = Config::get('smtp_pass'); // SMTP Passwort
+			$mail->AddAddress(Request::post('email'));
+			$mail->SetFrom = Config::get('smtp_email', Session::get('user_name'));
+			$mail->Subject = Request::post('betreff');
+			$mail->Body = Request::post('nachricht')."\n\n Viele Grüße".Session::get('user_name');
+			
+			if(!$mail->Send())
+			{
+				Session::add('response_negative', 'Email wurde nicht versandt. Es ist ein Fehler aufgetreten: '.$mail->ErrorInfo);
+				//print_r($mail);
+			}
+			else
+			{
+				Session::add('response_positive', 'Email wurde versandt.');
+			}
+			Redirect::to('dozent/emailDozent');
+		
+		}catch (phpmailerException $e){
+				echo $e->getMessage();
+		}
+	}
 public function writeMailAll(){
 		$this->View->render ( 'dozent/emailAll', array ('profil' => DozentModel::getDozentProfil(Session::get('user_name')),
 				'id' => Request::post ( 'id' )
 		) );
-		
-		print_r(array ('profil' => DozentModel::getDozentProfil(Session::get('user_name')),
-				'id' => Request::post ( 'id' )));
 	
 }
-public function sendMailAll(){
-		$mail = new PHPMailer();
- 
-	echo Config::get('smtp_host');
-  //Absenderadresse der Email setzen
-  $mail->From = Config::get('smtp_email');
-  
- //Name des Abenders setzen
-  $mail->FromName = Request::post('nachname');
-  
-  echo "aöjsfdlkj: ".Request::post ( 'id' );
-  $userlist = array('userlist' => DozentModel::getTeilnehmer(Request::post ( 'id')));
-  
- 
-  print_r($userlist);
-  //Empfängeradresse setzen
-  foreach ( $userlist as $key => $value ) {
-					$this->{$key} = $value;
-				}
-  foreach ( $this->userlist as $key => $value ) {
-				$mail->AddAddress($value->Email);
-				}
 
-  $mail->AddBCC('kraus.kilian@googlemail.com');
-  
-  //Betreff der Email setzen
-  $mail->Subject = Request::post('betreff');
+public function sendMailAll(){
+		
+		$mail = new PHPMailer(true);
+
+		$mail->IsSMTP(); //Versand über SMTP festlegen
+		
+		try{
+			$mail->Host = Config::get('smtp_host'); //SMTP-Server setzen
+			$mail->SMTPDebug  = 2; // Debug information
+			echo Config::get('smtp_host');
+	
+			// Testing
+			$mail->SMTPAuth = true; //Authentifizierung aktivieren
+			$mail->Port = Config::get('smtp_port');
+			
+			// Server
+			$mail->Username = Config::get('smtp_user');  // SMTP Benutzername
+			$mail->Password = Config::get('smtp_pass'); // SMTP Passwort
+			
+			$userlist = array('userlist' => DozentModel::getTeilnehmer(Request::post ( 'id')));
  
-  //Text der EMail setzen
-  $mail->Body = Request::post('nachricht').Request::post('nachname');
-  
-  $mail->IsSMTP(); //Versand über SMTP festlegen
-  $mail->Host = Config::get('smtp_host'); //SMTP-Server setzen
-  
-  $mail->SMTPAuth = true;     //Authentifizierung aktivieren
-  $mail->Username = Config::get('smtp_user');  // SMTP Benutzername
-  $mail->Password = Config::get('smtp_pass'); // SMTP Passwort 
-  
-  //EMail senden und überprüfen ob sie versandt wurde
-  if(!$mail->Send())
-  {
-      Session::add('response_negative', 'Email wurde nicht versandt. Es ist ein Fehler aufgetreten: '.$mail->ErrorInfo);
-  }
-  else
-  {
-     Session::add('response_positive', 'Email wurde versandt.');
+			//print_r($userlist);
+			//Empfängeradresse setzen
+			foreach ( $userlist as $key => $value ) {
+						$this->{$key} = $value;
+					}
+			foreach ( $this->userlist as $key => $value ) {
+						$mail->AddAddress($value->Email);
+					}
+			
+			// zu Testzwecken
+			$mail->AddBCC('wysdam@googlemail.com');
+			//Betreff der Email setzen
+			$mail->Subject = Request::post('betreff');
+			$mail->SetFrom = Config::get('smtp_email', Session::get('user_name'));
+			$mail->Subject = Request::post('betreff');
+			$mail->Body = Request::post('nachricht')."\n\n Viele Grüße".Session::get('user_name');
+			
+			if(!$mail->Send())
+			{
+				Session::add('response_negative', 'Email wurde nicht versandt. Es ist ein Fehler aufgetreten: '.$mail->ErrorInfo);
+				//print_r($mail);
+			}
+			else
+			{
+				Session::add('response_positive', 'Email wurde versandt.');
+			}
+			$this->View->render ( 'dozent/teilnehmerListe', array (
+				'teilnehmer' => DozentModel::getTeilnehmer(Request::post ( 'id' ) ) 
+			) );
+		
+		}catch (phpmailerException $e){
+				echo $e->getMessage();
+		}
+	}
 }
-//Redirect::to('dozent/emailDozent');
-}}
 ?>
 		
 	
